b50563f4d0b3f1bbb2057b42107659092cb8c3c3
João Leonardo <joaolhd18@gmail.com>
2022-06-22 15:43:38 -0300

    Shadows test part 2 (a lot more are coming)

--- core/src/com/gdx/tia/effects/PostProcessingResource.java
+++ core/src/com/gdx/tia/effects/PostProcessingResource.java
@@ -1,36 +1,53 @@
 package com.gdx.tia.effects;
 
-import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.gdx.tia.element.World;
 import com.gdx.tia.screens.GameScreen;
 
 public class PostProcessingResource {
 
-    public void apply(Batch batch) {
-        applyShadows(batch);
+    private SpriteBatch postProcBatch;
+
+    public PostProcessingResource() {
+        postProcBatch = new SpriteBatch();
     }
 
-    private void applyShadows(Batch batch) {
-        FrameBuffer fbo = new FrameBuffer(
+    public void apply(Batch batch) {
+        postProcBatch = (SpriteBatch) batch;
+        applyShadows();
+    }
+
+    private void applyShadows() {
+        Texture texture = new Texture("textures/black.png");
+        /*FrameBuffer shadowsFB = new FrameBuffer(
                 Pixmap.Format.RGBA8888,
                 (int) GameScreen.ref.getCamera().viewportWidth,
                 (int) GameScreen.ref.getCamera().viewportHeight,
                 false
-        );
+        );*/
 
         ShaderProgram shader = GameScreen.ref.getShaderResource().getShadowShader();
 
-        fbo.begin();
-        batch.setShader(shader);
+        //shadowsFB.begin();
+
+        postProcBatch.setShader(shader);
         for (Sprite caster : World.currentStage.getShadowCasters()) {
-            caster.draw(batch);
+            postProcBatch.draw(texture,
+                    caster.getX() - caster.getWidth() * 1.5f,
+                    caster.getY() - caster.getHeight()/2,
+                    caster.getWidth() * 4,
+                    caster.getHeight() * 2);
         }
-        batch.setShader(null);
-        fbo.end();
+        postProcBatch.setShader(null);
+
+        //shadowsFB.end();
     }
+
+    // passar a fonte de luz para o fragment shader
+    // passar o pixel central do caster
 
 }
